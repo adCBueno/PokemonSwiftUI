c15462c14c94b434992bdb8bdc14cf519c5e88c0
adCBueno <adrianacc.bueno@gmail.com>
2023-07-21 15:54:40 -0400

    Detail Finished and Settings finished

--- PokemonSwiftUI/Views/Pokemon/PokemonDetail.swift
+++ PokemonSwiftUI/Views/Pokemon/PokemonDetail.swift
@@ -15,24 +15,39 @@ struct PokemonDetail: View {
     var pokemon: PokemonResponse.Pokemon
     
     var body: some View {
-        VStack {
-            KFImage(URL(string: String(format: ConstantVariables.pokemonImageURL, pokemon.id)))
-                .resizable()
-                .frame(width: 200, height: 200, alignment: .center)
+        GeometryReader { geometry in
+            VStack {
+                ZStack {
+                    Rectangle()
+                        .fill(.white)
+                        .frame(height: geometry.size.height / 3)
+                        .border(.gray, width: 3)
+                        .edgesIgnoringSafeArea(.all)
+                        .cornerRadius(15)
+                        .padding()
+                    KFImage(URL(string: String(format: ConstantVariables.pokemonImageURL, pokemon.id)))
+                        .resizable()
+                        .frame(width: 200, height: 200, alignment: .center)
+                        .padding([.bottom], 10)
+                }
+                
+                VStack {
+                    Text(viewModel.pokemonDetail?.name ?? "")
+                        .font(.title2)
+                        .padding()
+                    Text("Primary Type: " + (viewModel.pokemonDetail?.types.first?.type.name ?? ""))
+                    Text("Secondary Type: " + (viewModel.pokemonDetail?.types.dropFirst().first?.type.name ?? "None"))
+                    HStack {
+                        KFImage(URL(string: viewModel.pokemonDetail?.sprites.front_default ?? ""))
+                        KFImage(URL(string: viewModel.pokemonDetail?.sprites.front_shiny ?? ""))
+                    }
+                }
+                .frame(maxHeight: .infinity, alignment: .top)
                 .padding()
-            Text(viewModel.pokemonDetail?.name ?? "")
-                .font(.title2)
-            Text("Primary Type: " + (viewModel.pokemonDetail?.types.first?.type.name ?? ""))
-            Text("Secondary Type: " + (viewModel.pokemonDetail?.types.dropFirst().first?.type.name ?? "None"))
-            HStack {
-                KFImage(URL(string: viewModel.pokemonDetail?.sprites.front_default ?? ""))
-                KFImage(URL(string: viewModel.pokemonDetail?.sprites.front_shiny ?? ""))
+                .onAppear {
+                    viewModel.loadData(url: pokemon.url)
+                }
             }
-        }
-        .frame(maxHeight: .infinity, alignment: .top)
-        .padding()
-        .onAppear {
-            viewModel.loadData(url: pokemon.url)
         }
     }
 }
--- PokemonSwiftUI/Views/Setting/SettingsView.swift
+++ PokemonSwiftUI/Views/Setting/SettingsView.swift
@@ -11,26 +11,39 @@ struct SettingsView: View {
     @EnvironmentObject var loginViewModel: LoginViewModel
     
     var body: some View {
-        VStack {
-            PikachuImage(imageName: "profile")
-                .frame(width: 250, height: 250)
-            
-            Text("email: \(loginViewModel.userProfile.email)")
-                .padding([.bottom], 5)
-                .font(.title2)
-        }
         
-        Button("Logout") {
-            loginViewModel.logout()
+        GeometryReader { geometry in
+            VStack {
+                ZStack {
+                    Rectangle()
+                        .fill(.white)
+                        .frame(height: geometry.size.height / 3)
+                        .border(.yellow, width: 3)
+                        .edgesIgnoringSafeArea(.all)
+                        .cornerRadius(15)
+                        .padding()
+                    PikachuImage(imageName: "profile")
+                        .frame(width: 250, height: 250)
+                }
+                
+                VStack {
+                    Text("email: \(loginViewModel.userProfile.email)")
+                        .padding([.bottom], 5)
+                        .font(.title2)
+                    Button("Logout") {
+                        loginViewModel.logout()
+                    }
+                    .foregroundColor(.cyan)
+                    .padding()
+                    .frame(width: 150, height: 50)
+                    .border(.cyan)
+                    .cornerRadius(10)
+                    .font(.title2)
+                    .bold()
+                    .padding([.top, .bottom], 100)
+                }
+            }
         }
-        .foregroundColor(.cyan)
-        .padding()
-        .frame(width: 150, height: 50)
-        .border(.cyan)
-        .cornerRadius(10)
-        .font(.title2)
-        .bold()
-        .padding([.top, .bottom], 100)
         
     }
 }
