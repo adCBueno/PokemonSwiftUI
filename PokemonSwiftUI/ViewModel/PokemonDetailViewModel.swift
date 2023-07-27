0dffe14281de1f5abbb934ab0a9565dc417336d0
adCBueno <adrianacc.bueno@gmail.com>
2023-07-27 14:42:41 -0400

    Erase local storage

--- PokemonSwiftUI/ViewModel/PokemonDetailViewModel.swift
+++ PokemonSwiftUI/ViewModel/PokemonDetailViewModel.swift
@@ -6,38 +6,11 @@
 //
 
 import Foundation
+import RealmSwift
 
 class PokemonDetailViewModel: ObservableObject {
     @Published var pokemonDetail: PokemonDetailResponse?
 
-    func getDocumentsDirectory() -> URL {
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        return paths[0]
-    }
-    
-    func saveData(_ pokemonDetail: PokemonDetailResponse, id: Int) {
-        do {
-            let filename = getDocumentsDirectory().appendingPathComponent("pokemon_\(id).json")
-            let data = try JSONEncoder().encode(pokemonDetail)
-            try data.write(to: filename)
-        } catch {
-            print("Unable to save data: \(error.localizedDescription)")
-        }
-    }
-    
-    func loadDataFromFile(id: Int) -> PokemonDetailResponse? {
-        let filename = getDocumentsDirectory().appendingPathComponent("pokemon_\(id).json")
-
-        do {
-            let data = try Data(contentsOf: filename)
-            return try JSONDecoder().decode(PokemonDetailResponse.self, from: data)
-        } catch {
-            print("Unable to load data: \(error.localizedDescription)")
-        }
-
-        return nil
-    }
-    
     func loadData(url: String, id: Int) {
         guard let url = URL(string: url) else {
             print("Invalid URL")
@@ -49,15 +22,11 @@ class PokemonDetailViewModel: ObservableObject {
                 if let decodedResponse = try? JSONDecoder().decode(PokemonDetailResponse.self, from: data) {
                     DispatchQueue.main.async {
                         self.pokemonDetail = decodedResponse
-                        self.saveData(decodedResponse, id: id)
                     }
                     return
                 }
             }
             print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
-            DispatchQueue.main.async {
-                self.pokemonDetail = self.loadDataFromFile(id: id)
-            }
         }
         task.resume()
     }
